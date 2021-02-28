f6da32c6b62e409814d112c0dfada3d5aec14efa
Dang Trung Hieu <hieudt230899@gmail.com>
2021-02-28 15:31:04 +0700

    Setup project, add cocoa pod, add third party pod.

---
+++ Tiki/Tiki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tiki
+//
+//  Created by Dang Trung Hieu on 2/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "HomView"
+        view.backgroundColor = .white
+    }
+
+
+}
+
