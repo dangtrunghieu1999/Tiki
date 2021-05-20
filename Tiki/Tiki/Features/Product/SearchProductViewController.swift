ee9ad0f4c10f680aaaecec7ee82dd89784ca4e1a
dangtrunghieu1999 <hieudt230899@gmail.com>
2021-05-20 09:33:16 +0700

    commit test 1

--- Tiki/Tiki/Features/Product/SearchProductViewController.swift
+++ Tiki/Tiki/Features/Product/SearchProductViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class SearchProductViewController: BaseViewController {
 
+    // MARK: - Define Variables
+    
+    fileprivate lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
     
     // MAKR: - View LifeCycles
     
